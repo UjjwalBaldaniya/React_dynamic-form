5addaf0186e77a8800f387142676d44484d96aa8
Ujjwal Baldaniya <ujjwalb.tagline@gmail.com>
2023-09-12 15:21:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Form from './Components/Form';
+// import Form from './Components/Form';
 import React from 'react'
 import NewForm from './Components/NewForm';
 
--- src/Components/NewForm.jsx
+++ src/Components/NewForm.jsx
@@ -1,13 +1,59 @@
 import React, { useState } from 'react'
 
+const data = [
+    {
+        lable: "Name :- ",
+        type: "text",
+        placeholder: "name",
+        name: "name"
+    },
+    {
+        lable: "Surname :- ",
+        type: "text",
+        placeholder: "surname",
+        name: "surname"
+    },
+    {
+        lable: "Email :- ",
+        type: "email",
+        placeholder: "email",
+        name: "email"
+    },
+]
+const nestedData = [
+    {
+        lable: "City :- ",
+        placeholder: "city",
+        name: "city"
+    },
+    {
+        lable: "State :- ",
+        type: "text",
+        placeholder: "state",
+        name: "state"
+    },
+    {
+        lable: "Pincode :- ",
+        type: "number",
+        placeholder: "pincode",
+        name: "pincode"
+    },
+]
+
 const NewForm = () => {
     const [formData, setFormData] = useState({
-        person: {
-            name: "ujjwal",
-            surname: "aa",
-            current: {
-                city: "surat",
-            },
+        personDetails: {
+            name: "",
+            surname: "",
+            email: "",
+        },
+    });
+
+    const [nestedForm, setNestedForm] = useState({
+        currentAddress: {
+            city: "",
+            state: "",
+            pincode: ""
         },
     });
 
@@ -15,19 +61,27 @@ const NewForm = () => {
         const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
-            person: {
-                ...prevData.person,
-                current: {
-                    ...prevData.person.current,
-                    [name]: value,
-                },
+            personDetails: {
+                ...prevData.personDetails,
+                [name]: value,
             },
         }));
     };
 
+    const handleNestedChange = (e) => {
+        const { name, value } = e.target;
+        setNestedForm((prevData) => ({
+            ...prevData,
+            currentAddress: {
+                ...prevData.currentAddress,
+                [name]: value,
+            },
+        }));
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        formData.personDetails.currentAddress = nestedForm.currentAddress
         console.log(formData);
     };
 
@@ -35,30 +89,18 @@ const NewForm = () => {
         <div>
             <h1>Form</h1>
             <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.person.name}
-                    onChange={handleChange}
-                />
-
-                <label>Surname:</label>
-                <input
-                    type="text"
-                    name="surname"
-                    value={formData.person.surname}
-                    onChange={handleChange}
-                />
-
-                <label>Current City:</label>
-                <input
-                    type="text"
-                    name="city"
-                    value={formData.person.current.city}
-                    onChange={handleChange}
-                />
-
+                {data.map((element, index) => (
+                    <div key={index}>
+                        <label>{element.lable}</label>
+                        <input {...element} value={formData.personDetails[element.name]} onChange={handleChange} />
+                    </div>
+                ))}
+                {nestedData.map((element, index) => (
+                    <div key={index}>
+                        <label>{element.lable}</label>
+                        <input {...element} value={nestedForm.currentAddress[element.name]} onChange={handleNestedChange} />
+                    </div>
+                ))}
                 <button type="submit">Submit</button>
             </form>
         </div>
