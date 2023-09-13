2ae9f5e16b1ceab8f1cb52c56b5ef374ed59b83f
Ujjwal Baldaniya <ujjwalb.tagline@gmail.com>
2023-09-13 18:50:41 +0530

    dynamic form complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import Form from './Components/Form';
 import React from "react";
-import NewForm from "./Components/NewForm";
+// import NewForm from "./Components/NewForm";
 import DynamicForm from "./Components/DynamicForm";
 
 const App = () => {
--- src/Components/DynamicForm.jsx
+++ src/Components/DynamicForm.jsx
@@ -1,40 +1,68 @@
 import React, { useState } from "react";
 
 const DynamicForm = () => {
-
     const formData = {
         name: { type: "text", name: 'name', placeholder: 'name', },
         email: { type: "email", name: 'email', placeholder: 'Entre email', },
-        // city:{type:"text", name:'city', placeholder:'Entre city', }
+        age: { type: "number", name: 'age', placeholder: 'Entre age', },
+        city: { type: "text", name: 'city', placeholder: 'Entre city', },
+        state: { type: "text", name: 'state', placeholder: 'Entre state', },
+        pincode: { type: "number", name: 'pincode', placeholder: 'Entre pincode', }
     }
     const [formFields, setFormFields] = useState(
         {
             name: '',
             email: '',
-            // currentAddress: {
-            //     city: '',
-            //     state: '',
-            //     pincode: '',
-            // },
+            age: '',
+            currentAddress: {
+                city: '',
+                state: '',
+                pincode: '',
+            },
+            permanentAddress: {
+                city: '',
+                state: '',
+                pincode: '',
+            },
         },
     )
 
     const handleFormChange = (e, index) => {
         const { name, value } = e.target
-        // console.log(name, index);
-        let data = formFields;
-        // console.log(data[name] = value);
-        setFormFields({
-            ...formFields,
-            [name]: value
-        })
-        // data[name] = value;
-        // setFormFields(data);
+        let data = { ...formFields };
+        if (name === 'name' || name === 'email' || name === "age") {
+            data = { ...data, [name]: value };
+        } else if (name === 'state' || name === 'city' || name === 'pincode') {
+            data = {
+                ...data,
+                currentAddress: {
+                    ...data.currentAddress,
+                    [name]: value
+                },
+                permanentAddress: {
+                    ...data.permanentAddress,
+                    [name]: value
+                },
+            };
+        }
+        setFormFields(data);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(formFields);
+        const dataToSend = {
+            personalDetails: formFields,
+        };
+        console.log(dataToSend);
+    }
+
+    const callBack = (index, key) => {
+        return (
+            <div key={index}>
+                <label>{key} :- </label>
+                <input value={formFields?.[key]} onChange={handleFormChange} {...formData?.[key]} />
+            </div>
+        )
     }
 
     return (
@@ -42,13 +70,19 @@ const DynamicForm = () => {
             <div className="App">
                 <form onSubmit={handleSubmit}>
                     {Object.entries(formFields).map(([key, value], index) => {
-                        console.log(`key = ${key} , value = ${value}, index= ${index}`);
-
+                        // console.log(`key = ${key} , value = ${value}, index= ${index}`);
                         return (
-                            <>
-                                <label>{key.toUpperCase()} :- </label>
-                                <input value={formFields?.[key]} onChange={handleFormChange} {...formData?.[key]} />
-                            </>
+                            <div key={index}>
+                                {key === "currentAddress" ?
+                                    Object.entries(formFields.currentAddress).map(([key, value], index) => {
+                                        return callBack(index, key)
+                                    })
+                                    : key === "permanentAddress" ? Object.entries(formFields.permanentAddress).map(([key, value], index) => {
+                                        return callBack(index, key)
+                                    })
+                                        : callBack(index, key)
+                                }
+                            </div>
                         )
                     })}
                     <button type="submit">Submit</button>
@@ -59,3 +93,5 @@ const DynamicForm = () => {
 };
 
 export default DynamicForm;
+
+
